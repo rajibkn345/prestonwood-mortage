5f7fb92b319028f0b4c29f804fb328d64691ef3c
Roman Khan Rajib <rajibkn345@gmail.com>
2022-11-15 21:58:15 +0600

    footer done

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { footer } from "../../data/Data"
+// import { footer } from "../../data/Data"
 import "./footer.css"
 
 const Footer = () => {
@@ -19,10 +19,32 @@ const Footer = () => {
 
       <footer>
         <div className='container'>
-          <div className="about-us">About Us</div>          
-          <div className="contact-us">Contact Us</div>          
-          <div className="Disclaimers">Disclaimers</div>          
-          <div className="Resources">Resources</div>          
+          <div className="about-us">
+            <h4>About Us</h4>
+            <p>We've been helping customers afford the home of their dreams for many years and we love what we do.</p>
+          </div>          
+          <div className="contact-us">
+            <h4>Contact Us</h4>
+            <p>15150 Preston Road, Suite 300 Dallas, TX75248 <br/>Phone:(800) 530-5457<br/>john@prestonwoodmortage.com</p>
+          </div>          
+          <div className="Disclaimers">
+            <h4 style={{marginBottom:'10px'}}>Disclaimers</h4>
+            <ul>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Legal</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Privacy Policy</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Accessibility Statement</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Site Map</li>
+            </ul>
+          </div>          
+          <div className="Resources">
+          <h4 style={{marginBottom:'10px'}}>Resources</h4>
+            <ul>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Loan Programs</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Loan Process</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>Mortage Basics</li>
+              <li style={{paddingBottom:'10px',borderBottom: "1px dashed currentColor"}}>FAQ</li>
+            </ul>  
+          </div>          
         </div>
       </footer>
       <div className='legal'>
