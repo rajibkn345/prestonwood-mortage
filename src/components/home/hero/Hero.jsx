5abc50281d8c33e2b054ba6beb94df6939cb50f4
Roman Khan Rajib <rajibkn345@gmail.com>
2022-11-16 20:32:05 +0600

    updating

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,14 +1,24 @@
 import React from "react"
-import Heading from "../../common/Heading"
+// import Heading from "../../common/Heading"
 import "./hero.css"
+import heroRight from'../../images/about.jpg'
+import banner1 from '../../images/banner-shape1-1.png'
+import banner2 from '../../images/banner-shape2-1.png'
 
 const Hero = () => {
   return (
     <>
       <section className='hero'>
-        <div className='container'>
+        <div className='flexLeft'>
+          <div className="hero-item">
+          <div style={{position:'relative'}} className="hero-bgImg">
+            <img src={banner1} className='circleLeft'  alt="banner-1"/>
+            <img src={banner2} className={'circleRight'} style={{right:'0',width:'350px',position:'absolute',height:'auto'}} alt='banner-2'/>
+          </div>
+            <h2>Find the right mortage for you!</h2>
+          </div>
+          <div className="hero-item"><img src={heroRight} alt="img"/></div>
           {/* <Heading title='Search Your Next Home ' subtitle='Find new & featured property located in your local city.' /> */}
-
           {/* <form className='flex'>
             <div className='box'>
               <span>City/Street</span>
@@ -35,4 +45,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
