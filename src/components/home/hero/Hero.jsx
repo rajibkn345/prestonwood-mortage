6436c1e1a3556aad922d4bfdad88ed28b5c156c0
Roman Khan Rajib <rajibkn345@gmail.com>
2022-11-17 01:37:55 +0600

    added

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 // import Heading from "../../common/Heading"
 import "./hero.css"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import heroRight from'../../images/about.jpg'
 import banner1 from '../../images/banner-shape1-1.png'
 import banner2 from '../../images/banner-shape2-1.png'
+import { Button } from "@mui/material";
 
 const Hero = () => {
   return (
@@ -16,7 +19,30 @@ const Hero = () => {
                <img src={banner2} className={'circleRight'} style={{right:'0',width:'350px',position:'absolute',height:'auto'}} alt='banner-2'/>
              </div>
              <h2 className="heroTitle">Find the right mortage for you!</h2>
-               
+            <div className="heroCalculator">
+              <span style={{padding:'10px 30px',fontSize:'20px',color:'#fff', backgroundColor:'green',marginLeft:"30px"}}>Calculators</span>
+            <Box
+              component="form"
+              sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+            <div>
+              <TextField label="Total Amount" id="outlined-size-normal" defaultValue="$30,000" />
+              <TextField label="Down Payment" id="outlined-size-normal" defaultValue="$6,000" />
+            </div>
+            <div>
+              <TextField label="Interest Rate" id="outlined-size-normal" defaultValue="4%" />
+              <TextField label="Mortage Period" id="outlined-size-normal" defaultValue="30" />
+            </div>
+            
+            <Button size='large' sx={{mx:1,fontSize:'18px',textTransform:'capitalize'}} color='success' variant='contained'>Calculate Mortage</Button>
+            <Button size='large' sx={{mx:1,fontSize:'18px',textTransform:'capitalize',backgroundColor:'#1d2636'}} variant='contained'>Call Us: (800) 530-5457</Button>
+            
+           </Box>
+            </div>
           </div>
           <div className="hero-item heroImgRight"></div>
           {/* <Heading title='Search Your Next Home ' subtitle='Find new & featured property located in your local city.' /> */}
